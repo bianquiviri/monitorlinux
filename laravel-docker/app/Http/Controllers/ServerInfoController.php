45820b354bf5eede171250ddd9b681a3cb7c5dd4
Jose Bianco <bianquiviri@gmail.com>
2026-04-28 11:47:17 +0200

    fix: corregir importación de SSHService y sintaxis en rutas
    - Agregar use App\Services\SSHService en ServerInfoController
    - Corregir falta de coma en ruta server.container.manage en web.php

--- laravel-docker/app/Http/Controllers/ServerInfoController.php
+++ laravel-docker/app/Http/Controllers/ServerInfoController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Services\ServerMonitoringService;
+use App\Services\SSHService;
 use Inertia\Inertia;
 
 class ServerInfoController extends Controller
